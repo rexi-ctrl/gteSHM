a12823a6dc2b82821e69187459a778950c7d75a4
Isahmee <78511288+rexi-ctrl@users.noreply.github.com>
2025-06-23 19:55:08 +0700

    Add files via upload

---
+++ core/config.py
@@ -0,0 +1,35 @@
+
+from web3 import Web3
+
+RPC_URL = "https://carrot.megaeth.com/rpc"
+ROUTER_ADDRESS = Web3.to_checksum_address("0x2e0ed91bb2b1b74d5b91f51ea99f05b2f5e35a85")
+
+ROUTER_ABI = [
+    {
+        "inputs": [
+            {"internalType": "uint256", "name": "amountIn", "type": "uint256"},
+            {"internalType": "uint256", "name": "amountOutMin", "type": "uint256"},
+            {"internalType": "address[]", "name": "path", "type": "address[]"},
+            {"internalType": "address", "name": "to", "type": "address"},
+            {"internalType": "uint256", "name": "deadline", "type": "uint256"}
+        ],
+        "name": "swapExactTokensForTokens",
+        "outputs": [
+            {"internalType": "uint256[]", "name": "amounts", "type": "uint256[]"}
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+BASE_TOKEN = Web3.to_checksum_address("0x768b22fc580d05fbdfefcfd7e462163832faca52")
+
+GTE_TOKENS = [
+    Web3.to_checksum_address("0x768b22fc580d05fbdfefcfd7e462163832faca52"),
+    Web3.to_checksum_address("0xe9b6e75c243b6100ffcb1c66e8f78f96feea727f"),
+    Web3.to_checksum_address("0xfaf334e157175ff676911adcf0964d7f54f2c424"),
+    Web3.to_checksum_address("0x073973ccc35761d7ddd48f0c6555914852671d17"),
+    Web3.to_checksum_address("0x10a6be7d23989d00d528e68cf8051d095f741145"),
+    Web3.to_checksum_address("0xfb1e310fee8f03844551ad3a62d55e7d0ab60e9e"),
+    Web3.to_checksum_address("0x10a4141bccfc69be90d2d2af058138876cce1ef8")
+]
